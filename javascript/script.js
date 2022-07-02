0afac981b41d57b8a0bc7c3d0ce0f51b78853df1
Daniel <danielesteban2004@gmail.com>
2022-07-02 02:57:19 -0500

    bugs fixed and starting language functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
 AOS.init();
 const igmail = document.querySelector("#gmail");
 
-igmail.addEventListener("click", function(e){
+igmail.addEventListener("click", function (e) {
     e.preventDefault();
     var sLink = "mailto:" + escape("danielesteban2004@gmail.com")
-     + "?subject=" + escape("Saludo")
-     + "&body=" + "Hola Dani!";
+        + "?subject=" + escape("Saludo")
+        + "&body=" + "Hola Dani!";
     window.location.href = sLink;
 })
 
@@ -13,34 +13,31 @@ igmail.addEventListener("click", function(e){
 const but = document.querySelector("#navToggle")
 const navMenu = document.querySelector(".navMenu")
 
-but.addEventListener("click", ()=>{
+but.addEventListener("click", () => {
     navMenu.classList.toggle("navVisible")
 })
 
 // Funcion de scroll
 
-function navScroll(a, b, c, d){
+function navScroll(a, b, c, d) {
     a.classList.add("active")
     b.classList.remove("active")
     c.classList.remove("active")
     d.classList.remove("active")
 }
-function navRScroll(a, b, c, d){
+function navRScroll(a, b, c) {
     a.style.setProperty("font-size", "10vw")
     a.style.setProperty("text-decoration", "none")
     b.style.setProperty("font-size", "8vw")
     b.style.setProperty("text-decoration", "underline #fff")
     c.style.setProperty("font-size", "8vw")
     c.style.setProperty("text-decoration", "underline #fff")
-    d.style.setProperty("font-size", "8vw")
-    d.style.setProperty("text-decoration", "underline #fff")
 }
 
-function flechita(a,b,c,d){
+function flechita(a, b, c) {
     a.style.setProperty("opacity", "100%")
     b.style.setProperty("opacity", "0%")
     c.style.setProperty("opacity", "0%")
-    d.style.setProperty("opacity", "0%")
 }
 
 const barra = document.querySelectorAll(".barra");
@@ -54,21 +51,19 @@ const observer = new IntersectionObserver(secciones => {
         if (section.isIntersecting) {
             if (section.target.getAttribute("id") == "about") {
                 navScroll(barra[0], barra[1], barra[2], barra[3]);
-                navRScroll(menuR[0], menuR[1], menuR[2], menuR[3]);
-                flechita(flechas[0], flechas[1], flechas[2], flechas[3]);
+                navRScroll(menuR[0], menuR[1], menuR[2]);
+                flechita(flechas[0], flechas[1], flechas[2]);
             } else if (section.target.getAttribute("id") == "abilities") {
                 navScroll(barra[1], barra[2], barra[3], barra[0]);
-                navRScroll(menuR[1], menuR[2], menuR[3], menuR[0]);
-                flechita(flechas[1], flechas[2], flechas[3], flechas[0]);
+                navRScroll(menuR[1], menuR[2], menuR[0]);
+                flechita(flechas[1], flechas[2], flechas[0]);
             }
             else if (section.target.getAttribute("id") == "contenidoProyectos") {
                 navScroll(barra[2], barra[0], barra[1], barra[3]);
-                navRScroll(menuR[2], menuR[0], menuR[1], menuR[3]);
-                flechita(flechas[2], flechas[0], flechas[1], flechas[3]);
-            } else if (section.target.getAttribute("id") == "footer"){
+                navRScroll(menuR[2], menuR[0], menuR[1]);
+                flechita(flechas[2], flechas[0], flechas[1]);
+            } else if (section.target.getAttribute("id") == "footer") {
                 navScroll(barra[3], barra[0], barra[1], barra[2]);
-                navRScroll(menuR[3], menuR[0], menuR[1], menuR[2]);            
-                flechita(flechas[3], flechas[0], flechas[1], flechas[2]);
             }
         }
     });
@@ -77,4 +72,4 @@ const observer = new IntersectionObserver(secciones => {
     threshold: .1
 });
 
-misElementos.forEach(elem => observer.observe(elem) );
+misElementos.forEach(elem => observer.observe(elem));
