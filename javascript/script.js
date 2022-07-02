360204ae9d83ea7835cdcf0bd9d0e61e7aacad38
Daniel <danielesteban2004@gmail.com>
2022-07-02 02:27:47 -0500

    scroll function added, general color redesign and cellphone menu redesigned

--- javascript/script.js
+++ javascript/script.js
@@ -16,3 +16,65 @@ const navMenu = document.querySelector(".navMenu")
 but.addEventListener("click", ()=>{
     navMenu.classList.toggle("navVisible")
 })
+
+// Funcion de scroll
+
+function navScroll(a, b, c, d){
+    a.classList.add("active")
+    b.classList.remove("active")
+    c.classList.remove("active")
+    d.classList.remove("active")
+}
+function navRScroll(a, b, c, d){
+    a.style.setProperty("font-size", "10vw")
+    a.style.setProperty("text-decoration", "none")
+    b.style.setProperty("font-size", "8vw")
+    b.style.setProperty("text-decoration", "underline #fff")
+    c.style.setProperty("font-size", "8vw")
+    c.style.setProperty("text-decoration", "underline #fff")
+    d.style.setProperty("font-size", "8vw")
+    d.style.setProperty("text-decoration", "underline #fff")
+}
+
+function flechita(a,b,c,d){
+    a.style.setProperty("opacity", "100%")
+    b.style.setProperty("opacity", "0%")
+    c.style.setProperty("opacity", "0%")
+    d.style.setProperty("opacity", "0%")
+}
+
+const barra = document.querySelectorAll(".barra");
+const menuR = document.querySelectorAll(".aResponsive")
+const flechas = document.querySelectorAll(".arrow")
+
+const misElementos = document.querySelectorAll(".observar");
+
+const observer = new IntersectionObserver(secciones => {
+    secciones.forEach((section) => {
+        if (section.isIntersecting) {
+            if (section.target.getAttribute("id") == "about") {
+                navScroll(barra[0], barra[1], barra[2], barra[3]);
+                navRScroll(menuR[0], menuR[1], menuR[2], menuR[3]);
+                flechita(flechas[0], flechas[1], flechas[2], flechas[3]);
+            } else if (section.target.getAttribute("id") == "abilities") {
+                navScroll(barra[1], barra[2], barra[3], barra[0]);
+                navRScroll(menuR[1], menuR[2], menuR[3], menuR[0]);
+                flechita(flechas[1], flechas[2], flechas[3], flechas[0]);
+            }
+            else if (section.target.getAttribute("id") == "contenidoProyectos") {
+                navScroll(barra[2], barra[0], barra[1], barra[3]);
+                navRScroll(menuR[2], menuR[0], menuR[1], menuR[3]);
+                flechita(flechas[2], flechas[0], flechas[1], flechas[3]);
+            } else if (section.target.getAttribute("id") == "footer"){
+                navScroll(barra[3], barra[0], barra[1], barra[2]);
+                navRScroll(menuR[3], menuR[0], menuR[1], menuR[2]);            
+                flechita(flechas[3], flechas[0], flechas[1], flechas[2]);
+            }
+        }
+    });
+}, {
+    rootMargin: "0px",
+    threshold: .1
+});
+
+misElementos.forEach(elem => observer.observe(elem) );
